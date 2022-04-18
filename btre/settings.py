a443d6dcc2b27b1bb7b6ea0cd3511511a6261aa1
rashminainwal01 <rashmi.nainwal@kiwitech.com>
2022-04-18 11:53:07 +0530

    committed

--- btre/settings.py
+++ btre/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'pages',
     'listings',
     'realtors',
+    'django.contrib.humanize',
+    'Authentication',
+    'contacts',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +82,14 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER' : 'postgres',
+        'PASSWORD':'rashi@99',
+        'HOST': 'localhost'
+
+
+
     }
 }
 
@@ -128,3 +137,16 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Media Folder Settings
+
+# media settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+#Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+   
+}
--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published','price','list_date','realtor')
+    list_display_links = ('id','title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',) #is_published is used for showing data in front end if we not click on it .it does not show any data in frontend
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price') #for searching the data
+    list_per_page = 25
+admin.site.register(Listing, ListingAdmin)
+
+
